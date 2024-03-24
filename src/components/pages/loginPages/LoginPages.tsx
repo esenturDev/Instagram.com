24f6173d6dbbed4887d7a9eb20612d183c3d2c9b
esenturDev <nurlanuuluesentur@gmail.com>
2024-03-24 20:19:21 +0600

    added login pages style

--- src/components/pages/loginPages/LoginPages.tsx
+++ src/components/pages/loginPages/LoginPages.tsx
@@ -1,44 +1,59 @@
-import { useNavigate } from 'react-router';
-import scss from './LoginPages.module.scss';
-import { useGetUsersQuery } from '../../../redux/api/auth';
-import { useState } from 'react';
+import { useNavigate } from "react-router";
+import scss from "./LoginPages.module.scss";
+import { useGetUsersQuery } from "../../../redux/api/auth";
+import { useState } from "react";
+import logo from '../../..//assets/photos/screenshot2.png'
 export const LoginPages = () => {
-  const {data} =  useGetUsersQuery();
-  // const [postLogin] =  usePostLoginMutation();
-  const navigate =  useNavigate();
+	const { data } = useGetUsersQuery();
+	// const [postLogin] =  usePostLoginMutation();
+	const navigate = useNavigate();
 
-  const [email,setEmail] = useState<string>("");
-  const [password,setPassword] = useState<string>("");
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
 
+	const resultUsers = data?.find(
+		(item) => item.email === email && item.password === password
+	);
 
-  const resultUsers = data?.find((item) => item.email === email && item.password === password);
+	const handleLogin = async () => {
+		// const newUsers = {
+		//   email,
+		//   password,
+		// };
+		if (resultUsers) {
+			// await postLogin(newUsers);
+			localStorage.setItem("auth", JSON.stringify(email));
+			navigate("/");
+		} else {
+			alert("Пароль или Email не правильный!");
+		}
+	};
 
-  const handleLogin = async () => {
-    // const newUsers = {
-    //   email,
-    //   password,
-    // };
-    if(resultUsers) {
-      // await postLogin(newUsers);
-      localStorage.setItem('auth', JSON.stringify(email))
-      navigate('/')
-    } else {
-      alert("Пароль или Email не правильный!")
-    }
-  }
-
-  return (
-    <div className={scss.loginPages}>
-      <div className="container">
-        <div className={scss.content}>
-          <div>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='email'/>
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password'/>
-            <button onClick={handleLogin}>Login</button>
-            <button onClick={() => navigate('/register')}>Register</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className={scss.loginPages}>
+			<div className="container">
+				<div className={scss.content}>
+					<div className={scss.loginPhotos}>
+						<img src={logo} alt="logo photo loginPages" />
+					</div>
+					<div className={scss.forms}>
+						<input
+							type="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							placeholder="email"
+						/>
+						<input
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							placeholder="password"
+						/>
+						<button onClick={handleLogin}>Login</button>
+						<button onClick={() => navigate("/register")}>Register</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
